e81586043e5b757ce9eb24967b164649059b7b18
BambooL <bambool@psu.edu>
2016-04-24 21:24:37 -0400

    headerchecker tested

--- bin/headerchecker.py
+++ bin/headerchecker.py
@@ -11,10 +11,13 @@ class t:
       yield v
     yield self.value
  
-def check_and(key, store_header):
+def check_and(key, store):
 	global store_header
+	store_header = store
 	headers = store_header[key]
 	allheaders = []
+	if (len(headers) == 1):
+		return True
 	for i in range(len(headers)):
 		if type(headers[i]) is list:
 			if (check_or(headers[i])):
@@ -33,7 +36,7 @@ def check_and(key, store_header):
 				return False
 	return True
 
-def check_or(headers):
+def check_or(headers):	
 	for i in range(len(headers)):
 		for j in range(len(headers) - i):
 			if common_ancestor(headers[i], headers[j]):
@@ -46,18 +49,20 @@ def check_or(headers):
 # {B3:[A2, C1],  B2:[B1],  A2:[A1], C1:[1], A1:[1]}  common_ancestor(B3, B2) -> False
 # {B3:[A3],  B2:[B1, A1],  B1:[1], A3:[A2], A2: [A1], A1:[1]} common_ancestor(B3, B2) -> True
 def common_ancestor(cell1, cell2):
-	global list1 = []
-	global list2 = []
+	global list1 
+	global list2 
+	list1 = []
+	list2 = []
 	add_node1(cell1)
 	add_node2(cell2)
 	return intersect(list1, list2)
 
 def add_node1(cell1):
-	if (cell1 in dicts.keys()):
-		for item in dicts[cell1]:
+	if (cell1 in store_header.keys()):
+		for item in store_header[cell1]:
 			if item != "1":
 				list1.append(item)
-			add_node(item)
+			add_node1(item)
 	else:
 		return
 			
@@ -66,7 +71,7 @@ def add_node2(cell1):
 		for item in store_header[cell1]:
 			if item != "1":
 				list2.append(item)
-			add_node(item)
+			add_node2(item)
 	else:
 		return
 
